28a503f775b1921195e9bfdc37d67ba1af152422
Hugo Jobling <hello@thisishugo.com>
2018-06-28 20:51:53 +0100

    Use more idiomatic ES201x syntax

--- examples/aggregate-multiple-requests.js
+++ examples/aggregate-multiple-requests.js
@@ -2,32 +2,32 @@
 // send it back as a single response.
 
 addEventListener('fetch', event => {
-    event.respondWith(fetchAndLog(event.request))
+  event.respondWith(fetchPrices(event.request))
 })
-  
-async function fetchAndLog(request) {
-    const init = {
-      method: 'GET',
-      headers: {'Authorization': 'XXXXXX', 'Content-Type': 'text/plain'}
-    }
-    const [btcResp, ethResp, ltcResp] = await Promise.all([
-      fetch('https://api.coinbase.com/v2/prices/BTC-USD/spot', init),
-      fetch('https://api.coinbase.com/v2/prices/ETH-USD/spot', init),
-      fetch('https://api.coinbase.com/v2/prices/LTC-USD/spot', init)
-    ])
-  
-    const btc = await btcResp.json()
-    const eth = await ethResp.json()
-    const ltc = await ltcResp.json()
-  
-    let combined = {}
-    combined['btc'] = btc['data'].amount
-    combined['ltc'] = ltc['data'].amount
-    combined['eth'] = eth['data'].amount
-  
-    const responseInit = {
-      status: 200,
-      headers: {'Content-Type': 'application/json'}
-    }
-    return new Response(JSON.stringify(combined), responseInit)
+
+async function fetchPrices(request) {
+  // Fetch all responses in parallel
+  const responses = await Promise.all([
+    fetch('https://api.coinbase.com/v2/prices/BTC-USD/spot'),
+    fetch('https://api.coinbase.com/v2/prices/ETH-USD/spot'),
+    fetch('https://api.coinbase.com/v2/prices/LTC-USD/spot')
+  ])
+
+  // Extract the desired data from the received responses in parallel
+  const [btc, eth, ltc] = await Promise.all(
+    responses.map(async res => {
+      const {
+        data: {amount}
+      } = await res.json()
+
+      return amount
+    })
+  )
+
+  const prices = {btc, eth, ltc}
+
+  return new Response(JSON.stringify(prices), {
+    status: 200,
+    headers: {'Content-Type': 'text/plain'}
+  })
 }
