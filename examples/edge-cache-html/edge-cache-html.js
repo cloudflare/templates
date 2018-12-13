9183d033e64b53cec5e94a395603f1fd5df0d00e
Patrick Meenan <pmeenan@chromium.org>
2018-12-13 14:35:13 -0500

    Initialize KV with a value when it hasn't been seeded.

--- examples/edge-cache-html/edge-cache-html.js
+++ examples/edge-cache-html/edge-cache-html.js
@@ -239,7 +239,10 @@ async function GetCurrentCacheVersion(cacheVer) {
   if (cacheVer === null) {
     cacheVer = await EDGE_CACHE.get('html_cache_version');
     if (cacheVer === null) {
+      // Uninitialized - first time through, initialize KV with a value
+      // Blocking but should only happen immediately after worker activation.
       cacheVer = 0;
+      await EDGE_CACHE.put('html_cache_version', cacheVer.toString());
     } else {
       cacheVer = parseInt(cacheVer);
     }
