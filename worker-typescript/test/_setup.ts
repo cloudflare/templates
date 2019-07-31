c84f065ea1a964d2a42acf1f5a78e79765464019
Avery Harnish <averyharnish@gmail.com>
2019-07-31 14:58:32 -0500

    Run prettier

--- worker-typescript/test/_setup.ts
+++ worker-typescript/test/_setup.ts
@@ -1,4 +1,4 @@
 // set up global namespace for worker environment
-import * as makeServiceWorkerEnv from "service-worker-mock";
-declare var global: any;
-Object.assign(global, makeServiceWorkerEnv());
+import * as makeServiceWorkerEnv from 'service-worker-mock'
+declare var global: any
+Object.assign(global, makeServiceWorkerEnv())
--- worker-typescript/test/handler.ts
+++ worker-typescript/test/handler.ts
@@ -1,13 +1,23 @@
-import { expect } from "chai";
-import { handleRequest } from "../src/handler";
+import { expect } from 'chai'
+import { handleRequest } from '../src/handler'
 
-describe("handler returns response with request method", () => {
-    const methods = ["GET", "HEAD", "POST", "PUT", "DELETE", "CONNECT", "OPTIONS", "TRACE", "PATCH"];
-    methods.forEach((method) => {
-        it(method, async () => {
-            const result = await handleRequest(new Request("/", {method}));
-            const text = await result.text();
-            expect(text).to.include(method);
-        });
-    });
-});
+describe('handler returns response with request method', () => {
+  const methods = [
+    'GET',
+    'HEAD',
+    'POST',
+    'PUT',
+    'DELETE',
+    'CONNECT',
+    'OPTIONS',
+    'TRACE',
+    'PATCH',
+  ]
+  methods.forEach(method => {
+    it(method, async () => {
+      const result = await handleRequest(new Request('/', { method }))
+      const text = await result.text()
+      expect(text).to.include(method)
+    })
+  })
+})
