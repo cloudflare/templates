ba8bfcb6c530520b729e269fc3233e576f8e768a
Ashley Lewis <ashleymichal@gmail.com>
2019-09-12 14:31:39 -0500

    Use mime/lite instead of custom content type fn
    * s/var/let/
    * remove prefix logic

--- worker-sites/.wrangler/static/assets.js
+++ worker-sites/.wrangler/static/assets.js
@@ -1,21 +1,19 @@
+import mime from 'mime/lite';
+
 /**
  * Fetch and log a request
  * @param {Request} request
  */
 async function handleStaticRequests(request) {
   try {
-    var parsedUrl = new URL(request.url)
-    var pathname = parsedUrl.pathname
-
-    // custom prefix logic could be generalized using a `prefix` config
-    // and asset manifest
-    pathname = pathname.replace('/workers', '')
+    let parsedUrl = new URL(request.url)
+    let pathname = parsedUrl.pathname
 
     // this would also be eliminated if the `asset manifest` was calculated
     // before upload
-    var path = normalize_path(pathname)
+    let path = normalize_path(pathname)
 
-    var contentType = determine_content_type(path)
+    let contentType = mime.getType(path)
 
     let body = await STATIC_CONTENT.get(path, 'arrayBuffer')
 
@@ -47,49 +45,5 @@ function normalize_path(path) { // TODO: handle this by generating Asset Manifes
     // normal path, no need to do anything!
   } else {
     return path
-  }
-}
-
-function determine_content_type(path) {
-  if (path.endsWith('html')) {
-    return 'text/html'
-  } else if (path.endsWith('css')) {
-    return 'text/css'
-  } else if (path.endsWith('ttf')) {
-    return 'application/font-sfnt'
-  } else if (path.endsWith('yml')) {
-    return 'text/yaml'
-  } else if (path.endsWith('eot')) {
-    return 'application/vnd.ms-fontobject'
-  } else if (path.endsWith('json')) {
-    return 'application/json'
-  } else if (path.endsWith('md')) {
-    return 'text/markdown'
-  } else if (path.endsWith('webm')) {
-    return 'video/webm'
-  } else if (path.endsWith('otf')) {
-    return 'application/font-sfnt'
-  } else if (path.endsWith('js')) {
-    return 'text/javascript'
-  } else if (path.endsWith('xml')) {
-    return 'text/xml'
-  } else if (path.endsWith('svg')) {
-    return 'image/svg+xml'
-  } else if (path.endsWith('scss')) {
-    return 'text/x-sass'
-  } else if (path.endsWith('woff')) {
-    return 'application/font-woff'
-  } else if (path.endsWith('woff2')) {
-    return 'font/woff2'
-  } else if (path.endsWith('png')) {
-    return 'image/png'
-  } else if (path.endsWith('jpg')) {
-    return 'image/jpeg'
-  } else if (path.endsWith('mp4')) {
-    return 'video/mp4'
-  } else if (path.endsWith('gif')) {
-    return 'image/gif'
-  } else {
-    return 'text/plain'
   }
 }
