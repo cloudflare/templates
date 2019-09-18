911c50bf7f619cab393d0d396fcc4fdaa6d99d98
Avery Harnish <averyharnish@gmail.com>
2019-09-18 10:03:17 -0500

    Use npm package to serve kv assets

--- worker-sites/.wrangler/index.js
+++ worker-sites/.wrangler/index.js
@@ -1,38 +1,24 @@
-// everything in /api should be considered user code
-import { handleApiRequests } from './api'
-
 // handleStaticRequests should deal with getting stuff out of KV
-import { handleStaticRequests } from './static/assets'
-// handleNotFound may take a single response to return; should have default.
-import { handleNotFound } from './static/notfound'
+import { AssetWorker } from "@cloudflare/kv-asset-handlers";
 
-/**
- * Here is where we might suggest users add API handlers.
- * These should execute based on a router that falls through
- * to remaining event listeners on undefined routes.
- */
-addEventListener('fetch', event => {
-	let response = await handleApiRequests(event)
+addEventListener("fetch", event => {
+  event.respondWith(handleRequest(event.request));
+});
 
-	if (response) event.respondWith(response)
-})
+async function handleRequest(request) {
+  const subworkers = [new AssetWorker()];
 
-/**
- * This is the meat of static site serving. If we want to encourage
- * users to manipulate response bodies with something like HTMLRewriter,
- * we might consider adding an example of that here; between getting the
- * response out of KV, and actually returning it to the eyeball.
- */
-addEventListener('fetch', event => {
-	let response = handleStaticRequests(event)
+  console.log(request.url)
+  let url = new URL(request.url);
+  if (url.pathname === "/") {
+    request = new Request(`${url}/index.html`)
+  }
 
-	if (response) event.respondWith(response)
-})
-
-/**
- * Finally, we need a catch-all NotFound handler. To start we can
- * have this default to just redirecting to 404.html, but it's also pretty
- * trivial to give the user custom control over this, as we have done here
- * for workers docs.
- */
-addEventListener('fetch', handleNotFound)
+  for (let i = 0; i < subworkers.length; i++) {
+    const subworker = subworkers[i];
+    if (await subworker.condition(request)) {
+      return await subworker.handler(request);
+    }
+  }
+  return new Response("not found", { status: 404, statusText: "not found" });
+}
