9667ffc34c9050ee0acce1fb2914baf1ed6f7598
Avery Harnish <averyharnish@gmail.com>
2019-09-18 10:48:05 -0500

    Run format

--- worker-sites/.wrangler/index.js
+++ worker-sites/.wrangler/index.js
@@ -8,11 +8,14 @@ addEventListener("fetch", event => {
 async function handleRequest(request) {
   let url = new URL(request.url);
   if (url.pathname === "/") {
-    request = new Request(`${url}/index.html`)
+    request = new Request(`${url}/index.html`);
   }
   try {
-    return await getAssetFromKV(request)
+    return await getAssetFromKV(request);
   } catch (e) {
-    return new Response(`"${url.pathname}" not found`, { status: 404, statusText: "not found" })
+    return new Response(`"${url.pathname}" not found`, {
+      status: 404,
+      statusText: "not found"
+    });
   }
 }
