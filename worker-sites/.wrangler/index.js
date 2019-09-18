197ad80aa955121232d0c3db4df4294422f15b1e
Avery Harnish <averyharnish@gmail.com>
2019-09-18 10:44:38 -0500

    Use new npm pkg api

--- worker-sites/.wrangler/index.js
+++ worker-sites/.wrangler/index.js
@@ -1,24 +1,18 @@
 // handleStaticRequests should deal with getting stuff out of KV
-import { AssetWorker } from "@cloudflare/kv-asset-handlers";
+import { getAssetFromKV } from "@cloudflare/kv-asset-handlers";
 
 addEventListener("fetch", event => {
   event.respondWith(handleRequest(event.request));
 });
 
 async function handleRequest(request) {
-  const subworkers = [new AssetWorker()];
-
-  console.log(request.url)
   let url = new URL(request.url);
   if (url.pathname === "/") {
     request = new Request(`${url}/index.html`)
   }
-
-  for (let i = 0; i < subworkers.length; i++) {
-    const subworker = subworkers[i];
-    if (await subworker.condition(request)) {
-      return await subworker.handler(request);
-    }
+  try {
+    return await getAssetFromKV(request)
+  } catch (e) {
+    return new Response(`"${url.pathname}" not found`, { status: 404, statusText: "not found" })
   }
-  return new Response("not found", { status: 404, statusText: "not found" });
 }
