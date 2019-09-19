fdc7bf43b22f84b039df5b550b5c57f3f3e116c3
Avery Harnish <averyharnish@gmail.com>
2019-09-19 15:24:47 -0500

    Formatting

--- worker-sites/workers-site/index.js
+++ worker-sites/workers-site/index.js
@@ -8,7 +8,7 @@ async function handleEvent(event) {
   try {
     return await getAssetFromKV(event);
   } catch (e) {
-    let pathname = new URL(event.request.url).pathname
+    let pathname = new URL(event.request.url).pathname;
     return new Response(`"${pathname}" not found`, {
       status: 404,
       statusText: "not found"
