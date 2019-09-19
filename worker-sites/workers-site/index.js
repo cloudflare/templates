20e699a1e521812a1177380ab5c4dad2040c62e2
Avery Harnish <averyharnish@gmail.com>
2019-09-19 18:50:02 -0500

    Update package name

--- worker-sites/workers-site/index.js
+++ worker-sites/workers-site/index.js
@@ -1,4 +1,4 @@
-import { getAssetFromKV } from "@cloudflare/kv-asset-handlers";
+import { getAssetFromKV } from "@cloudflare/kv-asset-handler";
 
 addEventListener("fetch", event => {
   event.respondWith(handleEvent(event));
