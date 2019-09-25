2d9c4970654bf83cc284e5eea1f812bc9440ff01
Avery Harnish <averyharnish@gmail.com>
2019-09-25 10:45:26 -0500

    Add debug option

--- worker-sites/workers-site/index.js
+++ worker-sites/workers-site/index.js
@@ -1,15 +1,31 @@
-import { getAssetFromKV } from '@cloudflare/kv-asset-handler'
+import { getAssetFromKV, defaultKeyModifier} from '@cloudflare/kv-asset-handler'
+
+// do not set to true in production!
+const DEBUG = false
 
 addEventListener('fetch', event => {
   event.respondWith(handleEvent(event))
 })
 
 async function handleEvent(event) {
+  const url = new URL(event.request.url)
   try {
-    return await getAssetFromKV(event)
+    let options = {}
+    if (DEBUG) {
+      options = {
+        cacheControl: {
+          bypassCache: true
+        }
+      }
+    }
+    return await getAssetFromKV(event, options)
   } catch (e) {
-    return new Response(e.message || e.toString(), {
-      status: 404,
-    })
+    if (DEBUG) {
+      return new Response(e.message || e.toString(), {
+        status: 404,
+      })
+    } else {
+      return new Response(`"${defaultKeyModifier(url.pathname)}" not found`, { status: 404 })
+    }
   }
 }
