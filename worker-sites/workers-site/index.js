7c5a15214ad33c99a75d487764f7170891aad729
Ashley Lewis <ashleymichal@gmail.com>
2019-09-26 12:58:19 -0500

    update template with customization comments and guides

--- worker-sites/workers-site/index.js
+++ worker-sites/workers-site/index.js
@@ -1,7 +1,13 @@
 import { getAssetFromKV, mapRequestToAsset } from '@cloudflare/kv-asset-handler'
 
-// do not set to true in production!
-const DEBUG = true
+/**
+ * The DEBUG flag will do two things that help during development:
+ * 1. we will skip caching on the edge, which makes it easier to
+ *    debug.
+ * 2. we will return an error message on exception in your Response rather
+ *    than the default 404.html page.
+ */
+const DEBUG = false
 
 addEventListener('fetch', event => {
   try {
@@ -20,18 +26,14 @@ addEventListener('fetch', event => {
 
 async function handleEvent(event) {
   const url = new URL(event.request.url)
-    let options = {
-      // customize how incoming requests will map to assets
-      mapRequestToAsset: request => {
-        // compute the key used by default (e.g. / -> index.html)
-        let defaultAssetKey = mapRequestToAsset(request)
-        let url = new URL(defaultAssetKey.url)
-        // strip the prefix from looking up /docs for all files
-        url.pathname = url.pathname.replace(/^\/docs/, '/')
-        // inherit all other props from the default request
-        return new Request(url.toString(), defaultAssetKey)
-      },
-    }
+    let options = {}
+
+    /**
+     * You can add custom logic to how we fetch your assets
+     * by configuring the function `mapRequestToAsset`
+     */
+    // options.mapRequestToAsset = handlePrefix(/^\/docs/)
+
   try {
     if (DEBUG) {
       // customize caching
@@ -50,5 +52,26 @@ async function handleEvent(event) {
         status: 404,
       })
     }
+  }
+}
+
+/**
+ * Here's one example of how to modify a request to
+ * remove a specific prefix, in this case `/docs` from
+ * the url. This can be useful if you are deploying to a
+ * route on a zone, or if you only want your static content
+ * to exist at a specific path.
+ */
+function handlePrefix(prefix) {
+  return request => {
+    // compute the default (e.g. / -> index.html)
+    let defaultAssetKey = mapRequestToAsset(request)
+    let url = new URL(defaultAssetKey.url)
+
+    // strip the prefix from the path for lookup
+    url.pathname = url.pathname.replace(prefix, '/')
+    
+    // inherit all other props from the default request
+    return new Request(url.toString(), defaultAssetKey)
   }
 }
