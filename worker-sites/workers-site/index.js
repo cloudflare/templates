f7430dd9e0ae5b556bef56818bb1a51a1bb537bc
Victoria <bernard.victoria22@gmail.com>
2019-09-26 10:53:34 -0500

    add a custom map asset for prefix use case

--- worker-sites/workers-site/index.js
+++ worker-sites/workers-site/index.js
@@ -1,7 +1,4 @@
-import {
-  getAssetFromKV,
-  defaultRequestKeyModifier,
-} from '@cloudflare/kv-asset-handler'
+import { getAssetFromKV, mapRequestToAsset } from '@cloudflare/kv-asset-handler'
 
 // do not set to true in production!
 const DEBUG = true
@@ -11,23 +8,31 @@ addEventListener('fetch', event => {
     event.respondWith(handleEvent(event))
   } catch (e) {
     if (DEBUG) {
-    return event.respondWith(new Response(e.message || e.toString(), {
-      status: 404,
-    }))
-  }
+      return event.respondWith(
+        new Response(e.message || e.toString(), {
+          status: 404,
+        }),
+      )
+    }
     event.respondWith(new Response('Internal Error', { status: 500 }))
   }
 })
 
+const myMapRequestToAsset = request => {
+  // start with the default map
+  let newRequest = mapRequestToAsset(request)
+  // strip the prefix from looking up all files
+  return new Request(newRequest.url.replace('/docs', ''), newRequest)
+}
 async function handleEvent(event) {
   const url = new URL(event.request.url)
   try {
-    let options = {}
+    let options = {
+      mapRequestToAsset: myMapRequestToAsset,
+    }
     if (DEBUG) {
-      options = {
-        cacheControl: {
-          bypassCache: true,
-        },
+      options.cacheControl = {
+        bypassCache: true,
       }
     }
     return await getAssetFromKV(event, options)
@@ -37,10 +42,9 @@ async function handleEvent(event) {
         status: 404,
       })
     } else {
-      return new Response(
-        `"${defaultRequestKeyModifier(event.request).url}" not found`,
-        { status: 404 },
-      )
+      return new Response(`"${mapRequestToAsset(url.pathname)}" not found`, {
+        status: 404,
+      })
     }
   }
 }
