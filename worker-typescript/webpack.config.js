7503f8e28fd1cb8b05d3fec6567b80536c730fe9
Avery Harnish <averyharnish@gmail.com>
2020-06-02 12:20:21 -0500

    remove unused source map

--- worker-typescript/webpack.config.js
+++ worker-typescript/webpack.config.js
@@ -3,12 +3,12 @@ const webpack = require('webpack')
 
 const mode = process.env.NODE_ENV || 'production'
 
+
 module.exports = {
   output: {
     filename: `worker.${mode}.js`,
     path: path.join(__dirname, 'dist'),
   },
-  devtool: 'source-map',
   mode,
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
