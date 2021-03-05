39deb3612f292a13eb0ddff3d4a5f0dc0e9f33fc
Joe Banks <joseph@josephbanks.me>
2021-03-05 21:29:40 +0000

    Migrate to itty-router for simplified route syntax

--- worker-router/index.js
+++ worker-router/index.js
@@ -1,13 +1,13 @@
-const { Router } = require('8track');
+import { Router } from 'itty-router'
 
 // Create a new router
-const router = new Router()
+const router = Router()
 
 /*
 Our index route, a simple hello world.
 */
-router.get`/`.handle(ctx => {
-  ctx.html("Hello, world! This is the root page of your Worker template.");
+router.get("/", () => {
+  return new Response("Hello, world! This is the root page of your Worker template.")
 })
 
 /*
@@ -16,18 +16,22 @@ URL.
 
 Try visit /example/hello and see the response.
 */
-router.get`/example/${'input'}`.handle(ctx => {
+router.get("/example/:text", ({ params }) => {
   // Decode text like "Hello%20world" into "Hello world"
-  let input = decodeURIComponent(ctx.params.input);
+  let input = decodeURIComponent(params.text)
 
   // Construct a buffer from our input
-  let buffer = Buffer.from(input, "utf8");
+  let buffer = Buffer.from(input, "utf8")
 
   // Serialise the buffer into a base64 string
-  let base64 = buffer.toString("base64");
+  let base64 = buffer.toString("base64")
 
   // Return the HTML with the string to the client
-  ctx.html(`<p>Base64 encoding: <code>${base64}</code></p>`);
+  return new Response(`<p>Base64 encoding: <code>${base64}</code></p>`, {
+    headers: {
+      "Content-Type": "text/html"
+    }
+  })
 })
 
 /*
@@ -39,19 +43,26 @@ Try the below curl command to send JSON:
 
 $ curl -X POST <worker> -H "Content-Type: application/json" -d '{"abc": "def"}'
 */
-router.post`/post`.handle(async ctx => {
-  let req = ctx.event.request;
-
+router.post("/post", async request => {
+  // Create a base object with some fields.
   let fields = {
-    "asn": req.cf.asn,
-    "colo": req.cf.colo
+    "asn": request.cf.asn,
+    "colo": request.cf.colo
   }
 
-  if (req.headers.get("Content-Type") === "application/json") {
-    fields["json"] = await req.json()
+  // If the POST data is JSON then attach it to our response.
+  if (request.headers.get("Content-Type") === "application/json") {
+    fields["json"] = await request.json()
   }
 
-  ctx.json(fields);
+  // Serialise the JSON to a string.
+  const returnData = JSON.stringify(fields, null, 2);
+
+  return new Response(returnData, {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
 })
 
 /*
@@ -60,14 +71,12 @@ above, therefore it's useful as a 404 (and avoids us hitting worker exceptions, 
 
 Visit any page that doesn't exist (e.g. /foobar) to see it in action.
 */
-router.all`(.*)`.handle(ctx => {
-  ctx.html("404, not found!", { status: 404 })
-})
+router.all("*", () => new Response("404, not found!", { status: 404 }))
 
 /*
 This snippet ties our worker to the router we deifned above, all incoming requests
 are passed to the router where your routes are called and the response is sent.
 */
 addEventListener('fetch', (e) => {
-  e.respondWith(router.getResponseForEvent(e))
+  e.respondWith(router.handle(e.request))
 })
