a3dea7e6fb803030c060d9881ff514e1cfb3ba78
Kristian Freeman <kristian@kristianfreeman.com>
2021-03-30 13:19:41 -0500

    Initial commit

---
+++ worker-websocket/README.md
@@ -0,0 +1,5 @@
+# websocket-template
+
+a simple template for working with websockets in cfw
+
+extremely wip and should not be used for production stuff, this is my temporary websocket playground
---
+++ worker-websocket/index.js
@@ -0,0 +1,64 @@
+import template from './template'
+
+let count = 0
+
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleSession(websocket) {
+  websocket.accept()
+  websocket.addEventListener("message", async ({ data }) => {
+    if (data === "CLICK") {
+      count += 1
+      websocket.send(JSON.stringify({ count, tz: new Date() }))
+    } else {
+      // An unknown message came into the server. Send back an error message
+      websocket.send(JSON.stringify({ error: "Unknown message received", tz: new Date() }))
+    }
+  })
+
+  websocket.addEventListener("close", async evt => {
+    // Handle when a client closes the WebSocket connection
+    console.log(evt)
+  })
+}
+
+const websocketHandler = async request => {
+  const headers = {
+    'Access-Control-Allow-Origin': '*'
+  }
+
+  if (request.method == "OPTIONS") {
+    return new Response("OK", { headers })
+  }
+
+  const upgradeHeader = request.headers.get("Upgrade")
+  if (upgradeHeader !== "websocket") {
+    return new Response("Expected websocket", { status: 400 })
+  }
+
+  const [client, server] = Object.values(new WebSocketPair())
+  await handleSession(server)
+
+  return new Response(null, {
+    status: 101,
+    webSocket: client
+  })
+}
+
+async function handleRequest(request) {
+  try {
+    const url = new URL(request.url)
+    switch (url.pathname) {
+      case '/':
+        return template()
+      case '/ws':
+        return websocketHandler(request)
+      default:
+        return new Response("Not found", { status: 404 })
+    }
+  } catch (err) {
+    return new Response(err.toString())
+  }
+}
