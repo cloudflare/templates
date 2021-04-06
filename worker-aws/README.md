ea39149a2d0c1142e55179ad690e5ec8b1874a7d
Cody Koeninger <ckoeninger@cloudflare.com>
2021-04-06 17:04:09 -0500

    Needs testing - integrate aurora example from https://github.com/ConzorKingKong/workers-auroradb

--- worker-aws/README.md
+++ worker-aws/README.md
@@ -13,13 +13,18 @@ wrangler generate projectname https://github.com/cloudflare/workers-aws-template
 cd projectname
 ```
 
-[`index.js`](https://github.com/cloudflare/workers-aws-template/blob/master/index.js) is the content of the Workers script. See the commented areas of the code for where to fill in your region, table name, etc.
+[`index.js`](https://github.com/cloudflare/workers-aws-template/blob/master/index.js) is the content of the Workers script. In handleRequest, uncomment the example for the service you want to try out.
 
-You'll need to use wrangler secrets to add appropriate values for AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, and AWS_SQS_QUEUE_URL, e.g.
+You'll need to use wrangler secrets to add appropriate values for AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, plus any of the service specific secrets, e.g.
 
 ```
 wrangler secret put AWS_ACCESS_KEY_ID
+wrangler secret put AWS_SECRET_ACCESS_KEY
+wrangler secret put AWS_AURORA_SECRET_ARN
+wrangler secret put AWS_SQS_QUEUE_URL
 ```
+
+Configuration of less sensitive values such as AWS_REGION can be done in wrangler.toml vars if you'd prefer.
 
 After that you can use `wrangler publish` as normal. See the [wrangler documentation](https://developers.cloudflare.com/workers/cli-wrangler) for more information.
 
--- worker-aws/index.js
+++ worker-aws/index.js
@@ -1,20 +1,11 @@
 const { DynamoDBClient, GetItemCommand, PutItemCommand } = require("@aws-sdk/client-dynamodb");
 const { SQSClient, SendMessageCommand } = require("@aws-sdk/client-sqs");
+const { RDSDataClient, ExecuteStatementCommand } = require("@aws-sdk/client-rds-data");
+
 
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
-
-async function handleRequest(request) {
-    await sqsExample();
-    const item = await dynamoExample();
-    return new Response(JSON.stringify(item), {
-    headers: { 'content-type': 'application/json' },
-  })
-}
-
-// replace with your region
-const myRegion = "us-west-2"
 
 async function myCredentialProvider() {
     return {
@@ -24,32 +15,26 @@ async function myCredentialProvider() {
     }
 }
 
-async function dynamoExample() {
-    const client = new DynamoDBClient({
-        region: myRegion,
-        credentialDefaultProvider: myCredentialProvider
-    });
+async function handleRequest(request) {
+    // The AWS SDK tries to use crypto from off of the window,
+    // so we need to trick it into finding it where it expects it
+    global.window = {}
+    window.crypto = crypto
 
-    // replace with your table name and key as appropriate
-    const put = new PutItemCommand({
-        TableName: "test_table_name",
-        Item: {
-            "greeting": { S: "Hello!" },
-            "my_primary_key": { S: "world" }
-        }
-    });
-    await client.send(put);
-    const get = new GetItemCommand({
-        TableName: "test_table_name",
-        Key: { "my_primary_key": { S: "world" } }
-    });
-    const results = await client.send(get);
-    return results.Item;
+    // uncomment the example you want to try out, comment the rest
+    const result =
+       await sqsExample();
+    // await dynamoExample();
+    // await auroraExample(request);
+
+    return new Response(JSON.stringify(result), {
+    headers: { 'content-type': 'text/plain' },
+  })
 }
 
 async function sqsExample() {
     const client = new SQSClient({
-        region: myRegion,
+        region: AWS_REGION,
         credentialDefaultProvider: myCredentialProvider
     });
 
@@ -60,4 +45,92 @@ async function sqsExample() {
     });
 
     return client.send(send);
+}
+
+async function dynamoExample() {
+    const client = new DynamoDBClient({
+        region: AWS_REGION,
+        credentialDefaultProvider: myCredentialProvider
+    });
+
+    // replace with your table name and key as appropriate
+    const put = new PutItemCommand({
+        TableName: AWS_DYNAMO_TABLE,
+        Item: {
+            "greeting": { S: "Hello!" },
+            "my_primary_key": { S: "world" }
+        }
+    });
+    await client.send(put);
+    const get = new GetItemCommand({
+        TableName: AWS_DYNAMO_TABLE,
+        Key: { "my_primary_key": { S: "world" } }
+    });
+    const results = await client.send(get);
+    return results.Item;
+}
+
+async function auroraExample(request) {
+  if (request.method === "POST") {
+    const jsonData = await request.json()
+    return await auroraPostData(jsonData)
+  } else {
+    // We need to create a URL object so we can read the query parameters from the request
+    const url = new URL(request.url)
+    const ID = url.searchParams.get("ID")
+    return await auroraGetData(ID)
+  }
+}
+
+async function auroraGetData(ID) {
+  const client = new RDSDataClient({
+    region: AWS_REGION,
+    credentialDefaultProvider: myCredentialProvider
+  });
+
+  const call = new ExecuteStatementCommand({
+    // This SQL command is susceptible to SQL Injections and
+    // is not production ready
+    sql: `SELECT * FROM ${AWS_AURORA_TABLE_NAME} WHERE id = ${ID};`,
+    resourceArn: AWS_AURORA_RESOURCE_ARN,
+    secretArn: AWS_AURORA_SECRET_ARN
+  })
+
+  const results = await client.send(call)
+
+  return results.records
+}
+
+async function auroraPostData(jsonData) {
+  const client = new RDSDataClient({
+    region: AWS_REGION,
+    credentialDefaultProvider: myCredentialProvider
+  });
+
+  const keysArray = Object.keys(jsonData)
+  let keys  = ""
+  let values = ""
+
+  keysArray.forEach((key, index) => {
+    keys += `${key}`
+    values += `'${jsonData[key]}'`
+
+    if (index !== keysArray.length - 1) {
+      keys += ", "
+      values += ", "
+    }
+  })
+
+  const call = new ExecuteStatementCommand({
+
+    // This SQL command is susceptible to SQL Injections and
+    // is not production ready
+    sql: `INSERT INTO ${AWS_AURORA_TABLE_NAME}(${keys}) VALUES (${values});`,
+    resourceArn: AWS_AURORA_RESOURCE_ARN,
+    secretArn: AWS_AURORA_SECRET_ARN
+  })
+
+  const results = await client.send(call)
+
+  return results
 }
