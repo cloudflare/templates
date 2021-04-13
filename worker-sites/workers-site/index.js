e191d6ebb5146392251f9e08ce3595f4f0efd142
Michael Jackson <michael@jackson.us>
2021-04-12 18:21:26 -0700

    Remove unused code
    The error handling example does not work correctly because neither
    `handleEvent` nor `event.respondWith` ever throw. Since `handleEvent`
    already takes care of handling errors with its own try/catch, it makes
    sense to leave the error handling code there.
    
    The `url` variable also was never used.

--- worker-sites/workers-site/index.js
+++ worker-sites/workers-site/index.js
@@ -10,22 +10,10 @@ import { getAssetFromKV, mapRequestToAsset } from '@cloudflare/kv-asset-handler'
 const DEBUG = false
 
 addEventListener('fetch', event => {
-  try {
-    event.respondWith(handleEvent(event))
-  } catch (e) {
-    if (DEBUG) {
-      return event.respondWith(
-        new Response(e.message || e.toString(), {
-          status: 500,
-        }),
-      )
-    }
-    event.respondWith(new Response('Internal Error', { status: 500 }))
-  }
+  event.respondWith(handleEvent(event))
 })
 
 async function handleEvent(event) {
-  const url = new URL(event.request.url)
   let options = {}
 
   /**
