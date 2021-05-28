04dda556e251b900a51c651ecbc1b59a7e44e377
Samuel Colvin <s@muelcolvin.com>
2021-05-28 16:00:40 +0100

    more vanilla settings as requested

--- worker-typescript/test/handler.test.ts
+++ worker-typescript/test/handler.test.ts
@@ -1,4 +1,4 @@
-import {handleRequest} from '../src/handler'
+import { handleRequest } from '../src/handler'
 import makeServiceWorkerEnv from 'service-worker-mock'
 
 declare var global: any
@@ -10,7 +10,7 @@ describe('handle', () => {
   })
 
   test('handle GET', async () => {
-    const result = await handleRequest(new Request('/', {method: 'GET'}))
+    const result = await handleRequest(new Request('/', { method: 'GET' }))
     expect(result.status).toEqual(200)
     const text = await result.text()
     expect(text).toEqual('request method: GET')
