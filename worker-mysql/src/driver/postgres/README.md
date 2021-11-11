2517ae84a1f2c25bec66698a517ebe7402d52114
Steve Manuel <smanuel@cloudflare.com>
2021-11-11 09:51:11 -0700

    feat: initial commit
    Co-authored-by: Ben Yule <byule@cloudflare.com>

--- worker-mysql/src/driver/postgres/README.md
+++
@@ -1,9 +0,0 @@
-This directory contains the source for the original bundled driver as well as artifcats created by 
-`edgeworkerizer.py` to enable it to run on Cloudflare Workers.
-
-### Bundle Deno PostgreSQL driver
-deno bundle https://deno.land/x/postgres@v0.13.0/mod.ts > postgres.js.deno
-
-python3 edgeworkerizer.py postgres.js.deno > postgres.js
-
-cp *.wasm ../../../dist/
--- worker-mysql/src/index.ts
+++ worker-mysql/src/index.ts
@@ -1,36 +1,33 @@
-import { Client } from './driver/postgres'
+import { Client } from './driver/mysql'
 
 export default {
   async fetch(request: Request, env, ctx: ExecutionContext) {
     // Add Cloudflare Access Service Token credentials as global variables, used when Worker
     // establishes the connection to Cloudflare Tunnel. This ensures only approved services can
     // connect to your Tunnel.
-    globalThis.CF_CLIENT_ID = env.CF_CLIENT_ID || undefined
-    globalThis.CF_CLIENT_SECRET = env.CF_CLIENT_SECRET || undefined
+    // globalThis.CF_CLIENT_ID = env.CF_CLIENT_ID || undefined
+    // globalThis.CF_CLIENT_SECRET = env.CF_CLIENT_SECRET || undefined
     // NOTE: You may omit these values, however your Tunnel will accept traffic from _any_ source
     // on the Internet which may result in extra load on your database.
 
     try {
       // Configure the database client and create a connection.
-      const client = new Client({
-        user: 'postgres',
-        database: 'postgres',
+      const mysql = new Client()
+      const mysqlClient = await mysql.connect({
+        username: 'user',
+        db: 'appdb',
         // hostname is the full URL to your pre-created Cloudflare Tunnel, see documentation here:
         // https://developers.cloudflare.com/cloudflare-one/connections/connect-apps/create-tunnel
-        hostname: 'https://dev.example.com',
+        hostname: env.TUNNEL_HOST || 'https://dev.example.com',
         password: 'password', // use a secret to store passwords
-        port: '5432',
       })
-      await client.connect()
 
       // Query the database.
       const param = 42
-      const result = await client.queryObject<
-        number
-      >`SELECT ${param} as answer;`
+      const result = await mysqlClient.query('SELECT ?;', [param])
 
       // Return result from database.
-      return new Response(JSON.stringify(result.rows[0]))
+      return new Response(JSON.stringify({ result }))
     } catch (e) {
       return new Response((e as Error).message)
     }
