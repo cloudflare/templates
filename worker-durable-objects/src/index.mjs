54cab8368c9c8291e7ea8b4bf2ce6d0ab5d84b94
Kenton Varda <kenton@cloudflare.com>
2022-02-07 12:28:58 -0600

    Improve comments around caching and gates

--- worker-durable-objects/src/index.mjs
+++ worker-durable-objects/src/index.mjs
@@ -27,18 +27,17 @@ export class Counter {
     // Apply requested action.
     let url = new URL(request.url);
 
-    // explanation of why this is safe and fast:
-    // https://blog.cloudflare.com/durable-objects-easy-fast-correct-choose-three/
+    // Durable Object storage is automatically cached in-memory, so reading the
+    // same key every request is fast. (That said, you could also store the
+    // value in a class member if you prefer.)
     let value = await this.state.storage.get("value") || 0;
 
     switch (url.pathname) {
     case "/increment":
       ++value;
-      await this.state.storage.put("value", value);
       break;
     case "/decrement":
       --value;
-      await this.state.storage.put("value", value);
       break;
     case "/":
       // Just serve the current value.
@@ -46,6 +45,12 @@ export class Counter {
     default:
       return new Response("Not found", {status: 404});
     }
+
+    // We don't have to worry about a concurrent request having modified the
+    // value in storage because "input gates" will automatically protect against
+    // unwanted concurrency. So, read-modify-write is safe. For more details,
+    // see: https://blog.cloudflare.com/durable-objects-easy-fast-correct-choose-three/
+    await this.state.storage.put("value", value);
 
     return new Response(value);
   }
