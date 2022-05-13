8320e34d5250494e08194540427c44727de09d70
Luke Edwards <luke.edwards05@gmail.com>
2022-05-12 18:45:14 -0700

    chore: prettier format

--- packages/workers.new/src/index.ts
+++ packages/workers.new/src/index.ts
@@ -6,26 +6,28 @@
 const source = 'github/cloudflare/templates/tree/main';
 
 const redirects: Record<string, [string, string]> = {
-  "/durable-objects": ["worker-durable-objects", "index.js"],
-  "/router": ["worker-router", "index.js"],
-  "/typescript": ["worker-typescript", "src/index.ts"],
-  "/websocket": ["worker-websocket", "index.js"],
-  "/worktop": ["worker-worktop", "src/index.ts"],
+  '/durable-objects': ['worker-durable-objects', 'index.js'],
+  '/router': ['worker-router', 'index.js'],
+  '/typescript': ['worker-typescript', 'src/index.ts'],
+  '/websocket': ['worker-websocket', 'index.js'],
+  '/worktop': ['worker-worktop', 'src/index.ts'],
 };
 
 const worker: ExportedHandler = {
   fetch(request) {
-    const { pathname } = new URL(request.url)
+    const { pathname } = new URL(request.url);
     const [subdir, file] = redirects[pathname] || [];
 
     if (subdir) {
-      return Response.redirect(`https://stackblitz.com/fork/${source}/${subdir}?file=${encodeURIComponent(file)}`, 302);
+      const focus = encodeURIComponent(file);
+      const target = `https://stackblitz.com/fork/${source}/${subdir}?file=${focus}`;
+      return Response.redirect(target, 302);
     }
 
     // See the wiki below for a description of this URL works.
     // https://wiki.cfops.it/display/ONB/How+to+use+Dashboard+Deep+Links
     return Response.redirect('https://dash.cloudflare.com/?to=/:account/workers/services/new', 301);
-  }
+  },
 };
 
 export default worker;
