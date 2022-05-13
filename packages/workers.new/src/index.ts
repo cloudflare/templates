dccf12c13cdd47b80ce179dc6948199545e147de
Luke Edwards <luke.edwards05@gmail.com>
2022-05-13 10:08:34 -0700

    import `workers.new` worker script (#70)
    * setup typescript worker
    
    * chore: import existing worker code
    
    * chore: use lookup table
    
    * chore: add worktop redirect
    
    * chore: prettier format
    
    * chore: add CD for worker
    
    * Update packages/workers.new/src/index.ts
    
    * Update .github/workflows/cd.workers-new.yml
    
    Co-authored-by: Adam Janiš <eidam@cloudflare.com>
    
    * save workflow changes
    
    Co-authored-by: Adam Janiš <eidam@cloudflare.com>

---
+++ packages/workers.new/src/index.ts
@@ -0,0 +1,31 @@
+// Redirect https://workers.new/<known> requests to IDE.
+// Redirect https://workers.new/*? requests to dashboard.
+// Similar to the concept of https://docs.new.
+
+// stackblitz repository source
+const source = 'github/cloudflare/templates/tree/main';
+
+const redirects: Record<string, [string, string]> = {
+  '/durable-objects': ['worker-durable-objects', 'index.js'],
+  '/router': ['worker-router', 'index.js'],
+  '/typescript': ['worker-typescript', 'src/index.ts'],
+  '/websocket': ['worker-websocket', 'index.js'],
+  '/worktop': ['worker-worktop', 'src/index.ts'],
+};
+
+const worker: ExportedHandler = {
+  fetch(request) {
+    const { pathname } = new URL(request.url);
+    const [subdir, file] = redirects[pathname] || [];
+
+    if (subdir) {
+      const focus = encodeURIComponent(file);
+      const target = `https://stackblitz.com/fork/${source}/${subdir}?file=${focus}`;
+      return Response.redirect(target, 302);
+    }
+
+    return Response.redirect('https://dash.cloudflare.com/?to=/:account/workers/services/new', 301);
+  },
+};
+
+export default worker;
