f1495161a351d9bcfd6ee385335db3c8c77dbbd5
Luke Edwards <luke.edwards05@gmail.com>
2022-05-12 18:42:02 -0700

    chore: use lookup table

--- packages/workers.new/src/index.ts
+++ packages/workers.new/src/index.ts
@@ -1,25 +1,29 @@
-// Requests to https://workers.new get redirected
-// to the dashboard to create a new Worker. Similar
-// to the concept of https://docs.new.
+// Redirect https://workers.new/<known> requests to IDE.
+// Redirect https://workers.new/*? requests to dashboard.
+// Similar to the concept of https://docs.new.
+
+// stackblitz repository source
+const source = 'github/cloudflare/templates/tree/main';
+
+const redirects: Record<string, [string, string]> = {
+  "/router": ["worker-router", "index.js"],
+  "/typescript": ["worker-typescript", "src/index.ts"],
+  "/durable-objects": ["worker-durable-objects", "index.js"],
+  "/websocket": ["worker-websocket", "index.js"],
+};
+
 const worker: ExportedHandler = {
-  async fetch(request) {
-    const url = new URL(request.url)
+  fetch(request) {
+    const { pathname } = new URL(request.url)
+    const [subdir, file] = redirects[pathname] || [];
 
-    let customRedirect
-    switch (url.pathname) {
-      case "/durable-objects": customRedirect = "https://stackblitz.com/fork/github/cloudflare/worker-examples/tree/main/worker-durable-objects?file=index.js&terminal=dev"; break;
-      case "/router": customRedirect = "https://stackblitz.com/fork/github/cloudflare/worker-examples/tree/main/worker-router?terminal=dev&file=index.js"; break;
-      case "/typescript": customRedirect = "https://stackblitz.com/fork/github/cloudflare/worker-examples/tree/main/worker-typescript?terminal=dev&file=src%2Findex.ts"; break;
-      case "/websocket": customRedirect = "https://stackblitz.com/fork/github/cloudflare/templates/tree/main/worker-websocket?file=index.js"; break;
-    }
-
-    if (customRedirect) {
-      return Response.redirect(customRedirect, 302)
+    if (subdir) {
+      return Response.redirect(`https://stackblitz.com/fork/${source}/${subdir}?file=${encodeURIComponent(file)}`, 302);
     }
 
     // See the wiki below for a description of this URL works.
     // https://wiki.cfops.it/display/ONB/How+to+use+Dashboard+Deep+Links
-    return Response.redirect('https://dash.cloudflare.com/?to=/:account/workers/services/new')
+    return Response.redirect('https://dash.cloudflare.com/?to=/:account/workers/services/new', 301);
   }
 };
 
