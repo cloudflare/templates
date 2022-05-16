8091655a4e53d4ad154f4364c086198665a9782c
Adam Janiš <eidam@cloudflare.com>
2022-05-16 18:43:47 +0100

    Update pages-functions-cors/functions/api/_middleware.ts
    Co-authored-by: Luke Edwards <luke.edwards05@gmail.com>

--- pages-functions-cors/functions/api/_middleware.ts
+++ pages-functions-cors/functions/api/_middleware.ts
@@ -1,6 +1,7 @@
 // Respond to OPTIONS method
 export const onRequestOptions: PagesFunction = async () => {
   return new Response(null, {
+    status: 204,
     headers: {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Headers': '*',
