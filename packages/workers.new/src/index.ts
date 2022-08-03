2e8c86c9378d858be3d2d200b12cf01ed47d0ee5
Adam Janiš <eidam@cloudflare.com>
2022-08-03 14:43:08 +0100

    init workers.new/list (#86)

--- packages/workers.new/src/index.ts
+++ packages/workers.new/src/index.ts
@@ -2,10 +2,12 @@
 // Redirect https://workers.new/*? requests to dashboard.
 // Similar to the concept of https://docs.new.
 
+type Redirects = Record<string, [string, string, string, string?]>;
+
 // stackblitz repository source
 const source = 'github/cloudflare/templates/tree/main';
 
-const redirects: Record<string, [string, string, string, string?]> = {
+const redirects: Redirects = {
 	'/durable-objects': ['worker-durable-objects', 'index.js', 'Workers Durable Objects counter'],
 	'/example-wordle': ['worker-example-wordle', 'src/index.ts', 'Workers Wordle example'],
 	'/router': ['worker-router', 'index.js', 'Workers router'],
@@ -22,18 +24,49 @@ const redirects: Record<string, [string, string, string, string?]> = {
 const worker: ExportedHandler = {
 	fetch(request) {
 		const { pathname } = new URL(request.url);
-		const [subdir, file, title, terminal] = redirects[pathname] || [];
 
-		if (subdir) {
-			const focus = encodeURIComponent(file);
-			const target = `https://stackblitz.com/fork/${source}/${subdir}?file=${focus}&title=${title}&terminal=${
-				terminal || 'start-stackblitz'
-			}`;
-			return Response.redirect(target, 302);
+		if (pathname === '/list') {
+			return new Response(getListHTML(redirects), { headers: { 'content-type': 'text/html' } });
+		}
+
+		const redirectUrl = getRedirectUrlForPathname(pathname);
+		if (redirectUrl) {
+			return Response.redirect(redirectUrl, 302);
 		}
 
 		return Response.redirect('https://dash.cloudflare.com/?to=/:account/workers/services/new', 302);
 	},
 };
+
+function getRedirectUrlForPathname(pathname: string): string | undefined {
+	const [subdir, file, title, terminal] = redirects[pathname] || [];
+	if (subdir) {
+		const focus = encodeURIComponent(file);
+		const target = `https://stackblitz.com/fork/${source}/${subdir}?file=${focus}&title=${title}&terminal=${
+			terminal || 'start-stackblitz'
+		}`;
+		return target;
+	}
+}
+
+function getListHTML(redirects: Redirects) {
+	return `
+<html>
+<head></head>
+<body>
+	<h1>List of workers.new redirects</h1>
+	<ul>
+		<li><a href="/">workers.new</a> - Cloudflare Dashboard shortcut</li>
+		${Object.keys(redirects)
+			.map(pathname => {
+				const [subdir, file, title, terminal] = redirects[pathname] || [];
+				return `<li><a href="${pathname}">workers.new${pathname}</a> - ${title}</li>`;
+			})
+			.join('\n')}
+	</ul>
+</body>
+</html>
+`;
+}
 
 export default worker;
