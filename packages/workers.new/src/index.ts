f7f7bc7565909f6bb5c0de8b0351ac899383324f
Brendan Irvine-Broque <birvine-broque@cloudflare.com>
2022-08-21 20:17:35 -0700

    Update redirects

--- packages/workers.new/src/index.ts
+++ packages/workers.new/src/index.ts
@@ -37,9 +37,11 @@ const redirects: Redirects = {
 		'Pages Plugin static forms',
 		'dev',
 	],
-	'/stream/video-js': ['stream/playback/video-js', 'src/index.html', 'Cloudflare Stream playback with Video.js'],
-	'/stream/vidstack': ['stream/playback/vidstack', 'src/index.html', 'Cloudflare Stream playback with Vidstack'],
-	'/stream/hls-js': ['stream/playback/hls-js', 'src/index.html', 'Cloudflare Stream playback with hls.js'],
+	'/stream/stream-player': ['stream/playback/stream-player', 'src/index.html', 'Cloudflare Stream Player'],
+	'/stream/video-js': ['stream/playback/video-js', 'src/index.html', 'Cloudflare Stream + Video.js'],
+	'/stream/vidstack': ['stream/playback/vidstack', 'src/index.html', 'Cloudflare Stream + Vidstack'],
+	'/stream/hls-js': ['stream/playback/hls-js', 'src/index.html', 'Cloudflare Stream + hls.js'],
+	'/stream/dash-js': ['stream/playback/dash-js', 'src/index.html', 'Cloudflare Stream + dash.js'],
 };
 
 const worker: ExportedHandler = {
