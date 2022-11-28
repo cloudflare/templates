bef83ffcf391b83d844ac77ec35174058307f451
Gabriel Massadas <gmassadas@cloudflare.com>
2022-11-28 18:25:28 +0000

    Add itty-router-openapi template

---
+++ worker-openapi/src/index.ts
@@ -0,0 +1,15 @@
+import {OpenAPIRouter} from '@cloudflare/itty-router-openapi'
+import {TaskCreate, TaskDelete, TaskFetch, TaskList} from './tasks'
+
+const router = OpenAPIRouter()
+router.get('/api/tasks/', TaskList)
+router.post('/api/tasks/', TaskCreate)
+router.get('/api/tasks/:taskSlug/', TaskFetch)
+router.delete('/api/tasks/:taskSlug/', TaskDelete)
+
+// 404 for everything else
+router.all('*', () => new Response('Not Found.', {status: 404}))
+
+export default {
+		fetch: router.handle
+}
