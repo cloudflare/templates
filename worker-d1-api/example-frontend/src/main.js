eedf309a5a386147dd2fb2b914a08254b84ae789
Kristian Freeman <kristian@kristianfreeman.com>
2022-12-01 11:18:22 -0600

    Format

--- worker-d1-api/example-frontend/src/main.js
+++ worker-d1-api/example-frontend/src/main.js
@@ -1,11 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-import './assets/water.css'
+import './assets/water.css';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+app.use(router);
 
-app.mount('#app')
+app.mount('#app');
