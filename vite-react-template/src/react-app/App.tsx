523d2324ff0e02d1a3339cf4eb64bf7d7e873f09
jdelorey@cloudflare.com <jdelorey@cloudflare.com>
2025-04-03 15:45:43 -0500

    chore: fix ci lints

--- vite-react-template/src/react-app/App.tsx
+++ vite-react-template/src/react-app/App.tsx
@@ -3,8 +3,8 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import cloudflareLogo from './assets/Cloudflare_Logo.svg'
-import honoLogo from './assets/hono.svg'
+import cloudflareLogo from "./assets/Cloudflare_Logo.svg";
+import honoLogo from "./assets/hono.svg";
 import "./App.css";
 
 function App() {
@@ -20,11 +20,15 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-        <a href='https://hono.dev/' target='_blank'>
-          <img src={honoLogo} className='logo cloudflare' alt='Hono logo' />
+        <a href="https://hono.dev/" target="_blank">
+          <img src={honoLogo} className="logo cloudflare" alt="Hono logo" />
         </a>
-        <a href='https://workers.cloudflare.com/' target='_blank'>
-          <img src={cloudflareLogo} className='logo cloudflare' alt='Cloudflare logo' />
+        <a href="https://workers.cloudflare.com/" target="_blank">
+          <img
+            src={cloudflareLogo}
+            className="logo cloudflare"
+            alt="Cloudflare logo"
+          />
         </a>
       </div>
       <h1>Vite + React + Hono + Cloudflare</h1>
@@ -54,9 +58,7 @@ function App() {
           Edit <code>worker/index.ts</code> to change the name
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the logos to learn more</p>
     </>
   );
 }
