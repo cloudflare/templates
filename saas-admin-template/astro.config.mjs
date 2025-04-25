26df62bbc224a1cb1de2d4af5e3429e7756274e4
spersad <spersad@cloudflare.com>
2025-04-25 12:35:22 -0400

    remove more explicit references to wrangler.json/c

--- saas-admin-template/astro.config.mjs
+++ saas-admin-template/astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
-      configPath: "wrangler.json",
+      configPath: "wrangler.jsonc",
       persist: {
         path: "./.cache/wrangler/v3",
       },
