c0c8b61ce3be5e8c77dc0c51582535eb8503e0c4
Gabriel Massadas <gmassadas@cloudflare.com>
2025-08-25 11:41:30 +0100

    fix nlweb

--- nlweb-template/src/ask/summarize.ts
+++ nlweb-template/src/ask/summarize.ts
@@ -1,55 +1,70 @@
 import type { StreamingWrapper } from "./stream-wrapper";
 import type { ResultBatch, MessageResponse } from "./types";
+import { MODEL } from "../config";
 
 export const summarize = async ({
-  query,
-  answers,
-  ai,
+	query,
+	answers,
+	ai,
 }: {
-  query: string;
-  answers: ResultBatch["results"];
-  ai: Ai;
+	query: string;
+	answers: ResultBatch["results"];
+	ai: Ai;
 }) => {
-  const response = await ai.run("@cf/openai/gpt-oss-20b", {
-    input: `
-                Given the following items, summarize the results as an answer to the user's question. 
-                The user's question is: ${query}. 
-                The items are: ${JSON.stringify(answers)}.`,
-  });
+	let response: any
+	try {
+		response = await ai.run(MODEL, {
+			messages: [
+				{
+					role: "user",
+					content: `
+										Given the following items, summarize the results as an answer to the user's question. 
+										The user's question is: ${query}. 
+										The items are: ${JSON.stringify(answers)}.`,
+				},
+			]
+    });
+  } catch (e) {
+		throw new Error(`${MODEL}: ${(e as Error).message}`)
+	}
 
-  const summary = response.output.at(-1)?.content[0]?.text as string;
-
-  return summary;
+	return response.response;
 };
 
 export const summarizeStreaming = async ({
-  query,
-  answers,
-  stream,
-  ai,
+	query,
+	answers,
+	stream,
+	ai,
 }: {
-  query: string;
-  answers: ResultBatch["results"];
-  stream?: StreamingWrapper;
-  ai: Ai;
+	query: string;
+	answers: ResultBatch["results"];
+	stream?: StreamingWrapper;
+	ai: Ai;
 }) => {
-  try {
-    const summary = await summarize({
-      query,
-      answers,
-      ai,
-    });
+	try {
 
-    stream?.writeStream({
-      message_type: "summary",
-      message: summary,
-      query_id: "",
-    } satisfies MessageResponse);
-  } catch (error) {
-    stream?.writeStream({
-      message_type: "error",
-      message: (error as Error).message,
-    });
-    console.error((error as Error).message);
-  }
+		const summary = await summarize({
+            query,
+            answers,
+            ai,
+        })
+
+		await stream?.writeStream({
+			message_type: "summary",
+			message: summary,
+			query_id: "",
+		} satisfies MessageResponse);
+	} catch (error) {
+		await stream?.writeStream({
+				message_type: "error",
+				message: (error as Error).message
+		})
+		await stream?.writeStream({
+			message_type: "summary",
+			message: (error as Error).message,
+			query_id: "",
+		})
+		console.error((error as Error).message);
+	}
 };
