0e97e00ecddf5083dc39e6aa4f4c5289cc835162
emily-shen <eshen@cloudflare.com>
2025-10-03 09:47:42 +0100

    update incorrect env var in containers template

--- containers-template/container_src/main.go
+++ containers-template/container_src/main.go
@@ -13,9 +13,9 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	message := os.Getenv("MESSAGE")
-	instanceId := os.Getenv("CLOUDFLARE_DEPLOYMENT_ID")
-
+	instanceId := os.Getenv("CLOUDFLARE_DURABLE_OBJECT_ID")
 	fmt.Fprintf(w, "Hi, I'm a container and this is my message: \"%s\", my instance ID is: %s", message, instanceId)
+
 }
 
 func errorHandler(w http.ResponseWriter, r *http.Request) {
